7aae5b1599ba907bbbf101d45ed347e2a89610ed
姬建明 <jijianming@MacBookPro2020.local>
2022-07-20 14:48:36 +0800

    完成接口逻辑

--- toexcel/views/toexcel_views.py
+++ toexcel/views/toexcel_views.py
@@ -1,8 +1,8 @@
 from rest_framework.response import Response
 from rest_framework import viewsets,mixins
 from django.contrib.auth import get_user_model
-
-
+from toexcel.libs import excel_tools
+import time
 User = get_user_model()
 
 
@@ -12,6 +12,12 @@ class ToExcelViews(viewsets.ViewSet, mixins.ListModelMixin):
 
 
     def list(self, request, *args, **kwargs):
-            data = {}
-            return Response(data)
+        filename = request.GET.get('filename', None)
+        if not filename:
+            data = {'code': 500, 'msg': '缺少关键参数filename', 'data': ''}
+        else:
+            day = time.strftime('%Y-%m-%d')
+            obj = excel_tools.ExcelTool(filename)
+            data = obj.add(day)
+        return Response(data)
 
